a83fc40fd2cdb55015b496b3f487c17290277d32
Dietmar Malli <github@malli.co.at>
2020-04-05 03:25:45 +0200

    implemented windowed and unwindowed classifier by doing a lot of refactoring, output of featurelists, output of plots

--- OfflineDataAnalysis/ClassificationTest.py
+++ OfflineDataAnalysis/ClassificationTest.py
@@ -1,70 +1,26 @@
-from Utils import feature_vectors, feature_vector, CLASSES, NR_TRAINING_FILES, my_parse_csv
-from sklearn.neighbors import KNeighborsClassifier
-import math
+from Utils import get_windowed_feature_lists, prepare_classification_result_plot, export_feature_list,\
+    filter_feature_vectors_to_common_number, get_classifier, get_unwindowed_feature_lists, CLASSES
 import matplotlib.pyplot as plt
-import numpy as np
 
 LEARN_WINDOW_TIME_S = 1.2
-TEST_WINDOW_TIME_S = LEARN_WINDOW_TIME_S
-K = 3
+TEST_WINDOW_TIME_S = 1.2
 
-feature_vectors_per_class = dict()
-for cnt_class in range(len(CLASSES)):
-    class_name = CLASSES[cnt_class]
-    for file_nr in range(NR_TRAINING_FILES):
-        print('Training on preprocessed/{}_{}.csv: '.format(class_name, file_nr), end='')
-        cur_time, cur_data = my_parse_csv('preprocessed/{}_{}.csv'.format(class_name, file_nr))
-        feature_vector_list = feature_vectors(cur_time, cur_data, LEARN_WINDOW_TIME_S)
-        print('Found {} feature vectors'.format(len(feature_vector_list)))
-        feature_vectors_per_class[cnt_class] = feature_vector_list
 
-common_nr_of_feature_vectors = math.inf
-for cnt_class, feature_vector_list in feature_vectors_per_class.items():
-    common_nr_of_feature_vectors = min(common_nr_of_feature_vectors, len(feature_vector_list))
-print('Common minimum of feature vectors for all classes={}'.format(common_nr_of_feature_vectors))
+# First generate a classifier with one feature vector per class:
+feature_vectors_per_class = get_unwindowed_feature_lists()
+export_feature_list('featurelists/unwindowed.json', feature_vectors_per_class)
+unwindowed_classifier = get_classifier(feature_vectors_per_class)
 
-X = []
-Y = []
-for cnt_class, feature_vector_list in feature_vectors_per_class.items():
-    for i in range(len(feature_vector_list)):
-        if i < common_nr_of_feature_vectors:
-            X.append(feature_vector_list[i])
-            Y.append(cnt_class)  # index of CLASSES is name to class nr mapping!
+# Then generate a classifier with more than one feature vector per class by windowing the preprocessed files:
+feature_vectors_per_class = get_windowed_feature_lists(LEARN_WINDOW_TIME_S)
+feature_vectors_per_class = filter_feature_vectors_to_common_number(feature_vectors_per_class)  # biased otherwise
+export_feature_list('featurelists/windowed.json', feature_vectors_per_class)
+windowed_classifier = get_classifier(feature_vectors_per_class)
 
-print('Using a total of {} feature vectors for KNN'.format(len(Y)))
-calculated_k = math.floor(math.sqrt(len(Y)))
-print('Calculated K={}'.format(calculated_k))
-clf = KNeighborsClassifier(n_neighbors=calculated_k)
-clf.fit(X, Y)
 
-current_test_file = 'preprocessed/walking_3.csv'
-x_time, x_data = my_parse_csv(current_test_file)
-test_vectors = feature_vectors(x_time, x_data, TEST_WINDOW_TIME_S)
-# predict/predict_proba expects an array and returns an array of results..
-all_probabilities = clf.predict_proba(test_vectors)
-transposed_probabilities = np.transpose(all_probabilities)
-window_nr = range(len(all_probabilities))
-fig, ((ax1, ax4), (ax2, ax5), (ax3, ax6)) = plt.subplots(3, 2)
-fig.suptitle('Probabilities for {}'.format(current_test_file))
-ax1.plot(window_nr, transposed_probabilities[0])
-ax1.set_title(CLASSES[0])
-ax2.plot(window_nr, transposed_probabilities[1])
-ax2.set_title(CLASSES[1])
-ax3.plot(window_nr, transposed_probabilities[2])
-ax3.set_title(CLASSES[2])
-ax4.plot(window_nr, transposed_probabilities[3])
-ax4.set_title(CLASSES[3])
-ax5.plot(window_nr, transposed_probabilities[4])
-ax5.set_title(CLASSES[4])
-ax6.plot(window_nr, transposed_probabilities[5])
-ax6.set_title(CLASSES[5])
-ax1.set_ylim([0, 1])
-ax2.set_ylim([0, 1])
-ax3.set_ylim([0, 1])
-ax4.set_ylim([0, 1])
-ax5.set_ylim([0, 1])
-ax6.set_ylim([0, 1])
-plt.show()
-# for cnt_class in range(len(CLASSES)):
-#     class_name = CLASSES[cnt_class]
-#     print('{}: {}%'.format(class_name, probabilities[cnt_class] * 100))
+# Generate plots showing performance:
+for classifier_name, classifier in {'unwindowed': unwindowed_classifier, 'windowed': windowed_classifier}.items():
+    for test_class in CLASSES:
+        current_test_file = 'preprocessed/{}_3.csv'.format(test_class)
+        prepare_classification_result_plot(classifier, current_test_file, TEST_WINDOW_TIME_S, classifier_name)
+        plt.savefig('classification_results/{}_{}.png'.format(test_class, classifier_name))
--- OfflineDataAnalysis/Utils.py
+++ OfflineDataAnalysis/Utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
+import math
+
+from sklearn.neighbors import KNeighborsClassifier
 
 MEAS_DATA_TIMESCALE = 1000 * 1000 * 1000  # ns/s
 CLASSES = ['upstairs', 'downstairs', 'walking', 'jogging', 'sitting', 'standing']
@@ -56,7 +60,7 @@ def feature_vector(transposed_data):
                      acc_x_var, acc_y_var, acc_z_var])
 
 
-def feature_vectors(time_data, transposed_data, window_length=0.5):
+def feature_vectors(time_data, transposed_data, window_length_s):
     un_transposed = np.transpose(transposed_data)
 
     features = []
@@ -64,7 +68,7 @@ def feature_vectors(time_data, transposed_data, window_length=0.5):
     begin_index = 0
     for current_index in range(len(time_data)):
         current_time = time_data[current_index] / MEAS_DATA_TIMESCALE
-        if current_time > window_length * next_window:
+        if current_time > window_length_s * next_window:
             current_data_window = un_transposed[begin_index:current_index]
             transposed_again = np.transpose(current_data_window)
             features.append(feature_vector(transposed_again))
@@ -72,3 +76,116 @@ def feature_vectors(time_data, transposed_data, window_length=0.5):
             next_window += 1
 
     return features
+
+
+def get_unwindowed_feature_lists():
+    feature_vectors_per_class = dict()
+    for cnt_class in range(len(CLASSES)):
+        class_name = CLASSES[cnt_class]
+        for file_nr in range(NR_TRAINING_FILES):
+            print('Training on preprocessed/{}_{}.csv (unwindowed)'.format(class_name, file_nr))
+            cur_time, cur_data = my_parse_csv('preprocessed/{}_{}.csv'.format(class_name, file_nr))
+            single_feature_vector = feature_vector(cur_data)
+            complete_feature_vector_for_current_class = []
+            if cnt_class in feature_vectors_per_class:
+                complete_feature_vector_for_current_class = feature_vectors_per_class[cnt_class]
+            complete_feature_vector_for_current_class.append(single_feature_vector)
+            feature_vectors_per_class[cnt_class] = complete_feature_vector_for_current_class
+    return feature_vectors_per_class
+
+
+def get_windowed_feature_lists(window_length_s):
+    feature_vectors_per_class = dict()
+    for cnt_class in range(len(CLASSES)):
+        class_name = CLASSES[cnt_class]
+        for file_nr in range(NR_TRAINING_FILES):
+            print('Training on preprocessed/{}_{}.csv: '.format(class_name, file_nr), end='')
+            cur_time, cur_data = my_parse_csv('preprocessed/{}_{}.csv'.format(class_name, file_nr))
+            feature_vector_list = feature_vectors(cur_time, cur_data, window_length_s)
+            print('Found {} feature vectors'.format(len(feature_vector_list)))
+            complete_feature_vector_for_current_class = []
+            if cnt_class in feature_vectors_per_class:
+                complete_feature_vector_for_current_class = feature_vectors_per_class[cnt_class]
+            complete_feature_vector_for_current_class.extend(feature_vector_list)
+            feature_vectors_per_class[cnt_class] = complete_feature_vector_for_current_class
+    return feature_vectors_per_class
+
+
+def filter_feature_vectors_to_common_number(feature_vectors_per_class):
+    new_feature_vectors_per_class = dict()
+
+    common_nr_of_feature_vectors = math.inf
+    for cnt_class, feature_vector_list in feature_vectors_per_class.items():
+        common_nr_of_feature_vectors = min(common_nr_of_feature_vectors, len(feature_vector_list))
+    print('Common minimum of feature vectors for all classes={}'.format(common_nr_of_feature_vectors))
+
+    for cnt_class, feature_vector_list in feature_vectors_per_class.items():
+        print('Filtering class {}, had {} vectors, '.format(CLASSES[cnt_class], len(feature_vector_list)), end='')
+        for i in range(len(feature_vector_list)):
+            if i == 0:
+                new_feature_vectors_per_class[cnt_class] = [feature_vector_list[0]]
+            elif i < common_nr_of_feature_vectors:
+                new_feature_vectors_per_class[cnt_class].append(feature_vector_list[i])
+            else:
+                break
+        print('now has {} vectors'.format(len(new_feature_vectors_per_class[cnt_class])))
+
+    return new_feature_vectors_per_class
+
+
+def export_feature_list(file_path, feature_vectors_per_class):
+    exportable_feature_vectors_per_class = dict()
+
+    for cnt_class, current_feature_vector_list in feature_vectors_per_class.items():
+        new_feature_vectors = []
+        for current_feature_vector in current_feature_vector_list:
+            new_feature_vectors.append(current_feature_vector.tolist())  # feature vectors are numpy arrays :S
+        exportable_feature_vectors_per_class[CLASSES[cnt_class]] = new_feature_vectors
+
+    with open(file_path, 'w') as file_handle:
+        json.dump(exportable_feature_vectors_per_class, file_handle, separators=(',', ':'), sort_keys=True, indent=4)
+
+
+def get_classifier(feature_vectors_per_class):
+    X = []
+    Y = []
+    for cnt_class, feature_vector_list in feature_vectors_per_class.items():
+        for i in range(len(feature_vector_list)):
+            X.append(feature_vector_list[i])
+            Y.append(cnt_class)  # index of CLASSES is name to class nr mapping!
+    print('Using a total of {} feature vectors for KNN'.format(len(Y)))
+    calculated_k = math.floor(math.sqrt(len(Y)))
+    print('Calculated K={}'.format(calculated_k))
+    classifier = KNeighborsClassifier(n_neighbors=calculated_k)
+    classifier.fit(X, Y)
+    return classifier
+
+
+def prepare_classification_result_plot(trained_classifier, test_file_path, test_window_time, classifier_name):
+    x_time, x_data = my_parse_csv(test_file_path)
+    test_vectors = feature_vectors(x_time, x_data, test_window_time)
+    # predict/predict_proba expects an array and returns an array of results..
+    all_probabilities = trained_classifier.predict_proba(test_vectors)
+    transposed_probabilities = np.transpose(all_probabilities)
+    window_nr = range(len(all_probabilities))
+    fig, ((ax1, ax4), (ax2, ax5), (ax3, ax6)) = plt.subplots(3, 2)
+    fig.suptitle('Probabilities for {} with {} classifier'.format(test_file_path, classifier_name))
+    ax1.plot(window_nr, transposed_probabilities[0])
+    ax1.set_title(CLASSES[0])
+    ax2.plot(window_nr, transposed_probabilities[1])
+    ax2.set_title(CLASSES[1])
+    ax3.plot(window_nr, transposed_probabilities[2])
+    ax3.set_title(CLASSES[2])
+    ax4.plot(window_nr, transposed_probabilities[3])
+    ax4.set_title(CLASSES[3])
+    ax5.plot(window_nr, transposed_probabilities[4])
+    ax5.set_title(CLASSES[4])
+    ax6.plot(window_nr, transposed_probabilities[5])
+    ax6.set_title(CLASSES[5])
+    ylimits = [-0.1, 1.1]
+    ax1.set_ylim(ylimits)
+    ax2.set_ylim(ylimits)
+    ax3.set_ylim(ylimits)
+    ax4.set_ylim(ylimits)
+    ax5.set_ylim(ylimits)
+    ax6.set_ylim(ylimits)
