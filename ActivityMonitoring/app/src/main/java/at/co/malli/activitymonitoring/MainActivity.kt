a31f12c0395cce6deadd9d04ff1962d8ad99517f
Dietmar Malli <github@malli.co.at>
2020-04-05 16:16:07 +0200

    most of boilerplate/moving for navigationUI done..

--- ActivityMonitoring/app/src/main/java/at/co/malli/activitymonitoring/MainActivity.kt
+++ ActivityMonitoring/app/src/main/java/at/co/malli/activitymonitoring/MainActivity.kt
@@ -1,6 +1,16 @@
 package at.co.malli.activitymonitoring
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.PowerManager
+import android.util.Log
+import android.view.View
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,8 +20,24 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    companion object {
+        private val TAG: String? = MainActivity::class.simpleName
+        val sensorDataList = LinkedList<SensorValue>() //static to survive orientation chg.
+        var externalFilesDir: File? = null
+
+        const val REQUEST_EXTERNAL_STORAGE_PERMISSION = 1
+    }
+
+    private lateinit var sm: SensorManager
+    private lateinit var wakeLock: PowerManager.WakeLock
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -26,14 +52,83 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_recording), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_recording
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        sm = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.v(TAG, "External storage permission is granted.")
+        } else {
+            Log.v(TAG, "Need to request external storage permission.")
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                REQUEST_EXTERNAL_STORAGE_PERMISSION
+            );
+        }
+
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(
+            PowerManager.PARTIAL_WAKE_LOCK,
+            "MyApp::MyWakelockTag"
+        )
+
+        val accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
+        wakeLock.acquire()
+
+        externalFilesDir = getExternalFilesDir(null)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_EXTERNAL_STORAGE_PERMISSION -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted
+                    Log.v(TAG, "External storage permission was granted right now!")
+                } else {
+                    // permission denied
+                    Log.v(TAG, "Need to request external storage permission AGAIN.")
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        REQUEST_EXTERNAL_STORAGE_PERMISSION
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        if (sensor != null)
+            Log.d(TAG, "Accuracy of sensor " + sensor.name + " changed to $accuracy")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            sensorDataList.add(
+                SensorValue(
+                    event.values[0], event.values[1], event.values[2], event.timestamp
+                )
+            )
+        }
     }
 }
--- ActivityMonitoring/app/src/main/java/at/co/malli/activitymonitoring/ui/recording/RecordingFragment.kt
+++ ActivityMonitoring/app/src/main/java/at/co/malli/activitymonitoring/ui/recording/RecordingFragment.kt
@@ -1,18 +1,43 @@
 package at.co.malli.activitymonitoring.ui.recording
 
+import android.app.Dialog
+import android.content.DialogInterface
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import at.co.malli.activitymonitoring.MainActivity.Companion.externalFilesDir
+import at.co.malli.activitymonitoring.MainActivity.Companion.sensorDataList
 import at.co.malli.activitymonitoring.R
+import at.co.malli.activitymonitoring.SensorValue
+import kotlinx.android.synthetic.main.fragment_recording.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.util.*
 
-class RecordingFragment : Fragment() {
+
+class RecordingFragment : Fragment(), View.OnClickListener {
+
+    companion object {
+        private val TAG: String? = RecordingFragment::class.simpleName
+
+        const val STATE_STOPPED = 0
+        const val STATE_RECORDING = 1
+        var currentState: Int = STATE_STOPPED
+    }
 
     private lateinit var recordingViewModel: RecordingViewModel
+    private lateinit var recordButton: Button
+    private lateinit var stopButton: Button
+
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,11 +46,63 @@ class RecordingFragment : Fragment() {
     ): View? {
         recordingViewModel =
                 ViewModelProviders.of(this).get(RecordingViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_recording, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        recordingViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        val rootView = inflater.inflate(R.layout.fragment_recording, container, false)
+        recordButton = rootView.findViewById(R.id.recordButton) as Button
+        stopButton = rootView.findViewById(R.id.stopButton) as Button
+        recordButton.setOnClickListener(this)
+        stopButton.setOnClickListener(this)
+
+        applyState()
+
+        return rootView
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.recordButton -> recordPressed()
+            R.id.stopButton -> stopPressed()
+        }
+    }
+
+    fun applyState(new_state: Int?) {
+        if (new_state != null)
+            currentState = new_state
+
+        if (currentState == STATE_RECORDING) {
+            recordButton.isEnabled = false
+            stopButton.isEnabled = true
+        } else {
+            recordButton.isEnabled = true
+            stopButton.isEnabled = false
+        }
+    }
+
+    fun recordPressed() {
+        Log.v(TAG, "recordPressed")
+        applyState(STATE_RECORDING)
+    }
+
+    fun stopPressed() {
+        applyState(STATE_STOPPED)
+        Log.v(TAG, "stopPressed")
+
+        if (sensorDataList.isEmpty()) {
+            Log.v(TAG, "List empty! Will not write anything!")
+            return
+        }
+
+        val lastTimestamp = sensorDataList.last.timestamp
+        val path = File(externalFilesDir, "$lastTimestamp.csv")
+        val fOutStream = OutputStreamWriter(FileOutputStream(path))
+        Log.v(TAG, "Opened file for writing: $path")
+        for (data in sensorDataList) {
+            val str = "${data.timestamp}, ${data.x}, ${data.y}, ${data.z}\n"
+            fOutStream.write(str)
+        }
+        fOutStream.flush()
+        fOutStream.close()
+        Log.v(TAG, "sensorDataList had ${sensorDataList.size} entries.")
+
+        sensorDataList.clear()
     }
 }
